1f9a54772324423ea1e00568a8ba406a2c3571e7
Walter <work.walter@gmail.com>
2014-11-12 09:49:24 +0000

    Implemented start_api.py for basic command line interaction with the program.

---
+++ start_api.py
@@ -0,0 +1,29 @@
+#!/user/bin/python3
+# encoding: utf-8
+
+"""
+start_api.py runs from the command line.
+Once started it takes command line arguments which are then passed to the 
+service module.
+"""
+
+import sys
+
+def process_stdin():
+    '''process_stdin() is a infinite loop which takes the stdin line by line
+       and processes the given commands on the service accordingly.
+       The function terminates by pressing Enter.
+    '''
+    try:
+        for line in sys.stdin:
+            if line.rstrip() == '':
+                break
+            for item in line.rstrip().split():
+                print('You entered:', item)
+        print('')
+    except Exception as msg:
+        print(msg)   
+
+
+if __name__ == '__main__':
+    process_stdin()
